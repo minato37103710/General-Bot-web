ba662d89f0ea6d05cb4f1bfbd34ea367ecfe83ab
minato37103710 <arigatoudane@outlook.jp>
2022-01-26 00:03:34 +0900

    update

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,4 @@
+const fetch = require('node-fetch');
 const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
@@ -11,19 +12,82 @@ async function main() {
     await mongoose.connect(process.env.mongourl);
 }
 
-router.get('/', function(req,res){
-    setting.findOne({server_name:'test3'}).then(d=>{
-        try{
-            if(d.test1){
-                console.log(d.server_name)}
-            else{
-                console.log('aaa')
-            }
-        }catch(e){
-            console.log('nul')}
-    })
-    res.render('index',{check:''})
-})
+async function oauth(res,req,code){
+    try {
+        const oauthResult = await fetch('https://discord.com/api/oauth2/token', {
+            method: 'POST',
+            body: new URLSearchParams({
+                client_id: '846989313291452437',
+                client_secret: '',
+                code,
+                grant_type: 'authorization_code',
+                redirect_uri: `http://localhost:3000/`,
+                scope: 'identify',
+            }),
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+            },
+        });
 
+        const oauthData = await oauthResult.json();
+        const userResult = await fetch('https://discord.com/api/users/@me', {
+            headers: {
+                authorization: `${oauthData.token_type} ${oauthData.access_token}`,
+            },
+        });
+        const userData=await userResult.json();
+        console.log(userData)
+    } catch (error) {
+        // NOTE: An unauthorized token will not throw an error;
+        // it will return a 401 Unauthorized response in the try block above
+        console.error(error);
+    }
+
+}
+
+async function refresh(){
+    try {
+        refreshResult = await fetch('https://discord.com/api/oauth2/token', {
+
+        method: 'POST',
+        body: new URLSearchParams({
+            client_id: '846989313291452437',
+            client_secret: '',
+            grant_type: 'refresh_token',
+            refresh_token: `${req.cookies.refreshtoken}`,
+        }),
+        headers: {
+            'Content-Type':'application/x-www-form-urlencoded',
+        },
+    });
+        userResult = await fetch('https://discord.com/api/users/@me', {
+            headers: {
+                authorization: `Bearer ${refreshData.access_token}`,
+            },
+        });
+    
+        userData=await userResult.json();
+    
+    } catch (error) {
+
+        console.error(error);
+}}
+    
+
+router.get('/', async(req, res) => {
+	const { code } = req.query;
+    if (code) {
+    switch (req.cookies.refreshtoken){
+        case 'undefined':
+            let oauthfunc=oauth(req,res,code);
+            console.log(oauthfunc)
+            res.json(oauthfunc)
+            break;
+    }
+    }else{
+        res.render('index',{login:'login'})
+    }
+    
+})
 
 module.exports = router;
